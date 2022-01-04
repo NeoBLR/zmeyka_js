d608f3863ce817db50042e7d058eaab022324da5
regman <regxxxman@gmail.com>
2022-01-04 23:37:39 +0300

    add move

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ class Snake {
     this.X = X
     this.Y = Y
     this.SnakeLen = SnakeLen
+    this.SnakeTails = [SnakeLen]
   }
 }
 
@@ -20,39 +21,72 @@ let Draw = (X, Y) => {
   //   }
 }
 
+let DrawSnake = (X, Y) => {
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext('2d')
+
+  ctx.clearRect(0, 0, 400, 400)
+
+  ctx.fillStyle = '#ff7675'
+
+  ctx.fillRect(16 * X, 16 * Y, 16, 16)
+}
+
+let MoveSide = 'UP'
+
 let main = () => {
   let PlayerSnake = new Snake(5, 5)
   Draw(PlayerSnake.X, PlayerSnake.Y)
+
+  //Move fn
+
+  let Move = () => {
+    if (MoveSide === 'UP') {
+      PlayerSnake.Y += -1
+      Draw(PlayerSnake.X, PlayerSnake.Y)
+    }
+    if (MoveSide === 'DOWN') {
+      PlayerSnake.Y += 1
+      Draw(PlayerSnake.X, PlayerSnake.Y)
+    }
+    if (MoveSide === 'RIGHT') {
+      PlayerSnake.X += -1
+      Draw(PlayerSnake.X, PlayerSnake.Y)
+    }
+    if (MoveSide === 'LEFT') {
+      PlayerSnake.X += 1
+      Draw(PlayerSnake.X, PlayerSnake.Y)
+    }
+
+    document.getElementById('x').innerHTML = PlayerSnake.X
+    document.getElementById('y').innerHTML = PlayerSnake.Y
+  }
+
+  //loop
+
+  let MoveLoop = setInterval(() => {
+    Move()
+  }, 1000)
 
   //Event
 
   document.addEventListener('keydown', (e) => {
     if (e.key === 'w') {
-      PlayerSnake.Y += -1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
+      MoveSide = 'UP'
     }
     if (e.key === 's') {
-      PlayerSnake.Y += 1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
+      MoveSide = 'DOWN'
     }
     if (e.key === 'a') {
-      PlayerSnake.X += -1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
+      MoveSide = 'RIGHT'
     }
     if (e.key === 'd') {
-      PlayerSnake.X += 1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
+      MoveSide = 'LEFT'
     }
 
-    // console.log(e)
     document.getElementById('KeyPress').innerHTML = e.key
       .toString()
       .toUpperCase()
-    // document.getElementById('xy').innerHTML =
-    //   'X: ' + PlayerSnake.X + ' Y: ' + PlayerSnake.Y
-
-    document.getElementById('x').innerHTML = PlayerSnake.X
-    document.getElementById('y').innerHTML = PlayerSnake.Y
   })
 }
 
