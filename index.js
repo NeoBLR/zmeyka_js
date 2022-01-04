658cf00872d05fee6f3005c49a4fb6623d1c12f5
regman <regxxxman@gmail.com>
2022-01-05 00:13:51 +0300

    Add Tail

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ class Snake {
   }
 }
 
+class Tail {
+  constructor(X, Y) {
+    this.X = X
+    this.Y = Y
+  }
+}
+
 let Draw = (X, Y) => {
   var canvas = document.getElementById('canvas')
   var ctx = canvas.getContext('2d')
 
-  ctx.clearRect(0, 0, 400, 400)
+  ctx.clearRect(0, 0, 400, 400) //clear all
   ctx.fillStyle = '#ff7675'
+
+  ctx.fillRect(16 * X, 16 * Y, 16, 16)
+
+  //   for (let i = 1; i < 5; i++) {
+  //     ctx.fillRect(16 * i, 16 * i, 16, 16)
+  //   }
+}
+
+let DrawTail = (X, Y) => {
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext('2d')
+
+  //   ctx.clearRect(0, 0, 400, 400) //clear all
+  ctx.fillStyle = '#ff7675b8'
 
   ctx.fillRect(16 * X, 16 * Y, 16, 16)
 
@@ -32,32 +53,43 @@ let DrawSnake = (X, Y) => {
   ctx.fillRect(16 * X, 16 * Y, 16, 16)
 }
 
+// setting
+
 let MoveSide = 'UP'
 
+let Speed = 1000
+
+// setting
+
 let main = () => {
-  let PlayerSnake = new Snake(5, 5)
+  let PlayerSnake = new Snake(5, 5, 1)
+  let SnakeTail = new Tail()
+
   Draw(PlayerSnake.X, PlayerSnake.Y)
 
   //Move fn
 
   let Move = () => {
-    if (MoveSide === 'UP') {
-      PlayerSnake.Y += -1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
-    }
-    if (MoveSide === 'DOWN') {
-      PlayerSnake.Y += 1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
-    }
-    if (MoveSide === 'RIGHT') {
-      PlayerSnake.X += -1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
-    }
-    if (MoveSide === 'LEFT') {
-      PlayerSnake.X += 1
-      Draw(PlayerSnake.X, PlayerSnake.Y)
-    }
+    SnakeTail.X = PlayerSnake.X
+    SnakeTail.Y = PlayerSnake.Y
 
+    if (MoveSide === 'UP') PlayerSnake.Y += -1
+
+    if (MoveSide === 'DOWN') PlayerSnake.Y += 1
+
+    if (MoveSide === 'RIGHT') PlayerSnake.X += -1
+
+    if (MoveSide === 'LEFT') PlayerSnake.X += 1
+
+    Draw(PlayerSnake.X, PlayerSnake.Y)
+    DrawTail(SnakeTail.X, SnakeTail.Y)
+
+    PrintInfo()
+  }
+
+  //PrintInfo
+
+  let PrintInfo = () => {
     document.getElementById('x').innerHTML = PlayerSnake.X
     document.getElementById('y').innerHTML = PlayerSnake.Y
   }
@@ -66,7 +98,7 @@ let main = () => {
 
   let MoveLoop = setInterval(() => {
     Move()
-  }, 1000)
+  }, Speed)
 
   //Event
 
