39b4c8ae9a41f1fd411f609671e2ebfeb497b194
neoblr <neoblr@bk.ru>
2022-01-06 10:31:10 +0300

    kek

--- index.js
+++ index.js
@@ -6,40 +6,68 @@ let Speed = 500
 
 let Score = 0
 
+let SnakeLen = 0
+
 // setting
 
 class Snake {
-  constructor(X, Y, SnakeLen) {
-    this.X = X
-    this.Y = Y
-    this.SnakeLen = SnakeLen
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    // this.SnakeLen = this.SnakeLen
     this.SnakeTails = [SnakeLen]
   }
 }
 
 class Tail {
-  constructor(X, Y) {
-    this.X = X
-    this.Y = Y
+  oldx = null
+  oldy = null
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  SetPosition = (x1, y1) => {
+    // if (this.oldx != null && this.oldy != null) {
+    //   this.oldx = this.x
+    //   this.oldy = this.y
+    // }
+
+    this.oldx = this.x
+    this.oldy = this.y
+
+    this.x = x1
+    this.y = y1
   }
 }
 
-let Draw = (X, Y) => {
+let Draw = (x, y) => {
   var canvas = document.getElementById('canvas')
   var ctx = canvas.getContext('2d')
 
   ctx.fillStyle = '#ff7675'
 
-  ctx.fillRect(16 * X, 16 * Y, 16, 16)
+  ctx.fillRect(16 * x, 16 * y, 16, 16)
 }
 
-let DrawTail = (X, Y) => {
+let DrawTail = (x, y) => {
   var canvas = document.getElementById('canvas')
   var ctx = canvas.getContext('2d')
 
-  ctx.fillStyle = '#ff7675b8'
+  ctx.fillStyle = '#75ffff17'
 
-  ctx.fillRect(16 * X, 16 * Y, 16, 16)
+  ctx.fillRect(16 * x, 16 * y, 16, 16)
+}
+
+let DrawTail1 = (mass) => {
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext('2d')
+
+  ctx.fillStyle = '#75ffff17'
+  console.log(mass)
+  for (let i = 0; i < mass.len; i++) {
+    ctx.fillRect(16 * x, 16 * y, 16, 16)
+  }
 }
 
 let Clear = () => {
@@ -50,9 +78,9 @@ let Clear = () => {
 }
 
 class Apple {
-  constructor(X, Y) {
-    this.X = X
-    this.Y = Y
+  constructor(x, y) {
+    this.x = x
+    this.y = y
   }
   DrawApple = () => {
     var canvas = document.getElementById('canvas')
@@ -60,7 +88,7 @@ class Apple {
 
     ctx.fillStyle = '#55efc4'
 
-    ctx.fillRect(16 * this.X, 16 * this.Y, 16, 16)
+    ctx.fillRect(16 * this.x, 16 * this.y, 16, 16)
   }
 
   IsScore = () => {
@@ -68,45 +96,82 @@ class Apple {
   }
 
   Colison = (x1, y1) => {
-    if (this.X === x1 && this.Y === y1) {
+    if (this.x === x1 && this.y === y1) {
       this.IsScore()
+      this.setRandomPosition()
       return true
     }
     return false
   }
+
+  setRandomPosition = () => {
+    this.x = Math.floor(Math.random() * 24)
+    this.y = Math.floor(Math.random() * 24)
+  }
 }
 
 let main = () => {
-  let PlayerSnake = new Snake(5, 5, 1)
-  let SnakeTail = [new Tail()]
+  let PlayerSnake = new Snake(5, 5)
+  let SnakeTail = []
 
   let PlayerApple = new Apple(6, 6)
 
-  Draw(PlayerSnake.X, PlayerSnake.Y)
+  Draw(PlayerSnake.x, PlayerSnake.y)
 
   //Move fn
 
   let Move = () => {
-    SnakeTail[0].X = PlayerSnake.X
-    SnakeTail[0].Y = PlayerSnake.Y
+    // Установка позицы
+    let SnakeTailPosition = () => {
+      for (let i = 0; i < SnakeLen; i++) {
+        SnakeTail[0].SetPosition(PlayerSnake.x, PlayerSnake.y)
+        if (!i == 0) {
+          // SnakeTail[0].x = PlayerSnake.x
+          // SnakeTail[0].y = PlayerSnake.y
 
-    if (MoveSide === 'UP') PlayerSnake.Y += -1
+          SnakeTail[i].SetPosition(SnakeTail[i - 1].x, SnakeTail[i - 1].y)
+        } else {
+        }
+      }
+    }
 
-    if (MoveSide === 'DOWN') PlayerSnake.Y += 1
+    SnakeTailPosition()
 
-    if (MoveSide === 'RIGHT') PlayerSnake.X += -1
+    if (MoveSide === 'UP') PlayerSnake.y += -1
 
-    if (MoveSide === 'LEFT') PlayerSnake.X += 1
+    if (MoveSide === 'DOWN') PlayerSnake.y += 1
+
+    if (MoveSide === 'RIGHT') PlayerSnake.x += -1
+
+    if (MoveSide === 'LEFT') PlayerSnake.x += 1
 
     Clear()
 
-    if (PlayerApple.Colison(PlayerSnake.X, PlayerSnake.Y)) {
-      SnakeTail.push(new Tail())
+    if (PlayerApple.Colison(PlayerSnake.x, PlayerSnake.y)) {
+      console.log('score + 100')
+
+      SnakeTail.push(new Tail(PlayerSnake.x, PlayerSnake.y))
+      SnakeLen += 1
+    }
+    console.log(SnakeTail)
+    
+    for (let i = 0; i < SnakeLen; i++) {
+      // console.log(
+      //   'SnakeTail = ' + i + ' x= ' + SnakeTail[i].x + ' y= ' + SnakeTail[i].y
+      // )
+      // if (i == 0) {
+      //   DrawTail(SnakeTail[i].x, SnakeTail[i].y)
+      // } else {
+      //   DrawTail(SnakeTail[i].oldx, SnakeTail[i].oldy)
+      // }
+
+      DrawTail(SnakeTail[i].x, SnakeTail[i].y)
     }
 
+    // DrawTail1(SnakeTail)
+
     PlayerApple.DrawApple()
-    Draw(PlayerSnake.X, PlayerSnake.Y)
-    DrawTail(SnakeTail[0].X, SnakeTail[0].Y)
+    Draw(PlayerSnake.x, PlayerSnake.y)
 
     PrintInfo()
   }
@@ -114,8 +179,10 @@ let main = () => {
   //PrintInfo
 
   let PrintInfo = () => {
-    document.getElementById('x').innerHTML = PlayerSnake.X
-    document.getElementById('y').innerHTML = PlayerSnake.Y
+    document.getElementById('x').innerHTML = PlayerSnake.x
+    document.getElementById('y').innerHTML = PlayerSnake.y
+
+    document.getElementById('lenspan').innerHTML = SnakeLen
 
     document.getElementById('score').innerHTML = Score
   }
